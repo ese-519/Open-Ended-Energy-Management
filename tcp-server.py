398c48d355182ea62ab0d5ee621f885982091f96
Ubuntu <ubuntu@ip-172-31-52-14.ec2.internal>
2016-11-05 18:40:33 +0000

    fixed ec2 tcp server query parsing error, send canned response to lambda

--- tcp-server.py
+++ tcp-server.py
@@ -31,7 +31,7 @@ def start_server(ipaddr, port):
       print >>sys.stderr, 'connection from', client_address
 
       # Receive the data in small chunks and retransmit it
-      query_from_lambda = []
+      query_parts= []
       while True:
         data = connection.recv(16)
         print >>sys.stderr, 'received "%s"' % data
@@ -39,18 +39,21 @@ def start_server(ipaddr, port):
 #TODO: remove          print >>sys.stderr, 'sending data back to the client'
 #          connection.sendall(data)
           # build query from lambda
-          query_from_lambda.append(data)
+          query_parts.append(data)
         else:
           print >>sys.stderr, 'no more data from', client_address
           # reconstruct query string
-          query_str = ''.append([s for s in query_from_lambda])
-          # TODO: parse query type and parameters
+          query_str = ''.join([s for s in query_parts])
+          # parse query type and parameters
           query = json.loads(query_str)
           print query
           # TODO: use subprocess.call to query KDD
           # TODO: wait for KDD query to complete
           # TODO: update DB with query result to update graphical output with pymongo.MongoClient
           # TODO: determine content for vocal response, send back to lambda using connection.sendall(some_data_here)
+	  response = {'param1': 'value1', 'param2': 2, 'param3': 'value3'}
+	  response_str = json.dumps(response)
+	  connection.sendall(response_str)
           break
             
     finally:
