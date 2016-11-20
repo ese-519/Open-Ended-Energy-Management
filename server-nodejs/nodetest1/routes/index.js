4f03892bbcf2eed4a241cd93cc94dc8054b33997
Ubuntu <ubuntu@ip-172-31-52-14.ec2.internal>
2016-11-20 19:34:08 +0000

    Status bar complete

--- server-nodejs/nodetest1/routes/index.js
+++ server-nodejs/nodetest1/routes/index.js
@@ -3,24 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-/* GET Hello World page. */
-router.get('/helloworld', function(req, res) {
-    res.render('helloworld', { title: 'Hello, World!' });
-});
-
-/* GET main page. */
-router.get('/temp', function(req, res) {
     var db = req.db;
     var collection = db.get('searchbin_results');
-    collection.find( { /* "_id" : "000000000000000000000001" */ },{},function(e,docs){
+    collection.find( {  "_id" : "000000000000000000000001" },{},function(e,docs){
         console.log("Resulting docs: " + JSON.stringify(docs))
         console.log("Resulting docs: " + JSON.stringify(docs[0]))
-        res.render('temp', {
-            "data" : docs
-        });
+        res.render('main', {
+            "data" : docs[0]
+         });
     });
 });
 
