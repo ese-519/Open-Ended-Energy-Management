2ee448daa68f76bfe8803908c5d93df7b9772eea
Ubuntu <ubuntu@ip-172-31-52-14.ec2.internal>
2016-11-21 01:49:40 +0000

    Integrated Highcharts-MongoDB

--- server-nodejs/nodetest1/routes/index.js
+++ server-nodejs/nodetest1/routes/index.js
@@ -1,16 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+var searchbin_data = null;
+var baseline_data = null;
+var db = null;
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
-    var db = req.db;
-    var collection = db.get('searchbin_results');
-    collection.find( {  "_id" : "000000000000000000000001" },{},function(e,docs){
-        console.log("Resulting docs: " + JSON.stringify(docs))
-        console.log("Resulting docs: " + JSON.stringify(docs[0]))
-        res.render('main', {
-            "data" : docs[0]
-         });
+    
+    function doRender()
+    {
+         if(searchbin_data !== null && baseline_data !== null)
+         {          
+            res.render('main', {
+                 "searchbin_data" : searchbin_data, "baseline_data" : baseline_data
+            });
+         }
+    }
+
+    db = req.db;
+    var searchbin_collection = db.get('searchbin_results');
+    searchbin_collection.find( { "_id" : "000000000000000000000001" },{},function(e,docs){
+         searchbin_data = docs[0];
+         console.log('searchbin_data: ' + JSON.stringify(searchbin_data))
+         console.log('docs: ' + JSON.stringify(docs[0]))
+         doRender();
+    });
+
+    var baseline_collection = db.get('baseline_data');
+    baseline_collection.find( { "_id" : 1 },{},function(e,docs2){
+           baseline_data = docs2[0];
+           console.log('baseline_data: ' + JSON.stringify(baseline_data))
+           console.log('docs: ' + JSON.stringify(docs2[0]))
+           doRender();
     });
 });
 
