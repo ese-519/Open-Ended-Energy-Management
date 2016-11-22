0b6f5cc3511e971448d9846977c4e0db85b403a5
blasagna <bdimaiolo@gmail.com>
2016-11-21 23:17:54 -0500

    adding lambda and backend code to handle predict month and predict day with plot update

--- MATLAB/DR-Baselining/baseline_july17.m
+++ MATLAB/DR-Baselining/baseline_july17.m
@@ -1,4 +1,4 @@
-function baseline_july
+function baseline_ka
 
 % disp('July 17th was a PLC day in 2013 for PJM.');
 % disp('Evaluating how well could we predict the baseline consumption');
@@ -12,9 +12,9 @@ load largetreeCVJul.mat
 % 5pm: 57228
 % 6pm: 57240
 % 7pm: 57252
-
-load YpredictCVJul.mat
-load date12numJul.mat
+load XtestPlc17.mat
+load YtestPlc17.mat
+load date12numPlc17.mat
 kf = 15
 % XtestPlc17 = Xtrain(56736:57023,:);
 % YtestPlc17 = Ytest(56736:57023);
@@ -23,18 +23,17 @@ kf = 15
 % Now use the cross validated trees to make predictions on the 2013 testing
 % data
 
-% YpredictCVkPlc17=zeros(length(XtestPlc17),kf);
-% for ii=1:kf
-%     YpredictCVkPlc17(:,ii)=predict(largetreeCVJul.Trained{ii,1},XtestPlc17);
-% end
-% YpredictPlc17 = sum(YpredictCVkPlc17,2)/kf;
+YpredictCVkPlc17=zeros(length(XtestPlc17),kf);
+for ii=1:kf
+    YpredictCVkPlc17(:,ii)=predict(largetreeCVJul.Trained{ii,1},XtestPlc17);
+end
+YpredictPlc17 = sum(YpredictCVkPlc17,2)/kf;
 
 response =[];
-response.y_predict = YpredictCVJul;
-response.time = date12numJul;
+response.y_predict = YpredictPlc17;
+response.time = date12numPlc17;
 savejson('',response,'Filename',['response.json']);
-% plot(response.time,response.y_predict)
-% % 
+% 
 % % RMSE
 % [a,b]=rsquare(YtestPlc17,YpredictPlc17);
 % fprintf('17th July 2013 RMSE(W): %.2f, R2: %.3f, RMSE/peak %0.4f, NRMSD: %0.2f \n\n'...
