8c65349b792391ec5c8cb47424425862dcacadcf
blasagna <bdimaiolo@gmail.com>
2017-02-05 15:48:53 -0500

    updated script to initially create lambda function

--- aws/deploy-lambda.sh
+++ aws/deploy-lambda.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -eux
 
+if (( $# != 1 )); then
+  echo "usage: ./deploy-lambda.sh <lambda role arn>"
+  exit
+fi
+LAMBDA_ROLE_ARN=$1
+
 zip lambda_code.zip lambda_function.py
 
 aws lambda create-function \
   --function-name "lambdaEnergyManagement" \
   --runtime "python2.7" \
-  --role "arn:aws:iam::031415712730:role/EnergyManagementLambdaRole" \
+  --role ${LAMBDA_ROLE_ARN} \
   --handler "lambda_function.lambda_handler" \
-  --zip-file "fileb://lambda_code.zip"
+  --zip-file "fileb://lambda_code.zip" \
   --description "Lambda function to handle requests from Alexa" \
   --timeout 3 \
   --memory-size 128
